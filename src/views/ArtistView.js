5a97a40b3b8d485cd9ce8b79b6ed6bee4a13e348
Alaa Alaghbar <johndoe@example.com>
2019-07-08 14:42:46 +0300

    added more styling

--- src/views/ArtistView.js
+++ src/views/ArtistView.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import gql from 'graphql-tag';
 import { graphql,Query } from 'react-apollo'
+import hit from './hit.png';
+import anathema1 from './anathema1.jpg';
+import Grid from '@material-ui/core/Grid';
+import pf from './pf.jpg';
+import scorpions from './scorpions.jpg';
 
 const query = gql`
 {
@@ -28,11 +33,74 @@ if (data !== undefined)
   return (
     <div style={{height:'100vh'}}>
       
-    <p style={{fontFamily:'BebasNeue Bold', color:'white', fontSize:'50px', position: 'absolute', float: 'right', textAlign: 'center'}}> AMP UP</p>
+    <p style={{ margin: '100px 50px 75px 200px', fontFamily:'BebasNeue Bold', color:'white', fontSize:'50px', position: 'absolute', float: 'right', textAlign: 'center'}}> AMP UP</p>
      <br></br>
      <br></br>
      <br></br>
-     <p style={{fontFamily:'BebasNeue Bold', color:'white', fontSize:'50px', position: 'absolute', float: 'right', textAlign: 'center'}}> THOSE DBS</p>
+     <p style={{margin: '100px 50px 75px 200px', fontFamily:'BebasNeue Bold', color:'white', fontSize:'50px', position: 'absolute', float: 'right', textAlign: 'center'}}> THOSE DBS</p>
+
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     
+     
+
+     <p style={{margin: '100px 50px 75px 200px', fontFamily:'BebasNeue Bold', color:'white', fontSize:'25px', position: 'absolute', float: 'right', textAlign: 'center'}}> MAKE MUSIC ON THE GO</p>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+
+     
+
+     <img src={hit} style={{margin: '100px 50px 75px 200px'}} alt="Logo" width="10%" />
+
+     {/* <img src={anathema} style={{margin: '50px 50px 175px 1000px'}} alt="Logo" width="10%" /> */}
+
+    <div style={{display: 'flex'}}>
+    
+  </div>
+
+
+     <Grid container spacing={3} style={{position: 'absolute', left: '700px', top: '150px', width: '40%'}}>
+        <Grid item xs={4}>
+        {/* <img src={anathema1}  alt="Logo" width="90%" /> */}
+
+
+        <div className="polaroid">
+  <img src={anathema1} alt="5 Terre" width="100%"/>
+  <div class="container">
+  <p>ANATHEMA</p>
+  </div>
+</div>
+
+
+
+        </Grid>
+        <Grid item xs={4}>
+
+
+
+        <div className="polaroid">
+  <img src={pf} alt="5 Terre" width="100%"/>
+  <div class="container">
+  <p>PINK FLOYD</p>
+  </div>
+</div>
+
+        
+        </Grid>
+        <Grid item xs={4}>
+        <div className="polaroid">
+  <img src={scorpions} alt="5 Terre" width="100%"/>
+  <div class="container">
+  <p>SCORPIONS</p>
+  </div>
+</div>
+        </Grid>
+        
+      </Grid>
 
       {data.artists.map((item,i) => {
         console.log(item)
